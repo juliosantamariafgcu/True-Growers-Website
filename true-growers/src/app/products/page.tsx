e68f95bf728886919fc893f227b73e68c6952bee
William Ward <wcward@gmail.com>
2025-08-26 13:03:54 -0400

    Update product list page

--- true-growers/src/app/products/page.tsx
+++ true-growers/src/app/products/page.tsx
@@ -1,5 +1,30 @@
+"use client"
+import { useEffect, useState } from "react";
+
+type Product = {
+  product_number: string;
+  title: string;
+  file_location: string;
+};
+
 export default function Products() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("product_data.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products))
+      .catch((err) => console.error("Error loading products:", err));
+  }, []);
+
   return (
-    <p>Products page</p>
+    <div>
+      <h1>Products</h1>
+      {products.map((product) => (
+        <div key={product.product_number}>
+          {product.product_number} - {product.title}
+        </div>
+      ))}
+    </div>
   );
 }
