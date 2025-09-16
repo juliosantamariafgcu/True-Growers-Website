5570ae7c70e83167f53e321c14ee86f7a992216a
William Ward <wcward@gmail.com>
2025-09-16 08:15:29 -0400

    Adding color mode based on OS detection

--- true-growers/src/app/layout.tsx
+++ true-growers/src/app/layout.tsx
@@ -2,10 +2,9 @@ import { Coda } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar";
 
-
 const coda = Coda({
   subsets: ["latin"],
-  weight: ["400", "800"] 
+  weight: ["400", "800"],
 });
 
 export default function RootLayout({
@@ -15,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${coda.className} bg-[#141414] text-white min-h-screen`}>
+      <body className={`${coda.className} bg-white text-black dark:bg-[#141414] dark:text-white min-h-screen`}>
         <Navbar />
         {children}
       </body>
--- true-growers/tailwind.config.ts
+++ true-growers/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "media",
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
